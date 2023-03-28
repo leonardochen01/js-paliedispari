e90a266577425679ada4bf443de9f1953e5b290b
leonardochen01 <leonardochen.lc@gmail.com>
2023-03-29 01:54:02 +0200

    Problem: can't continue

--- Pari-e-dispari/js/script.js
+++ Pari-e-dispari/js/script.js
@@ -12,27 +12,32 @@ const userSelectedNumber = document.getElementById("number");
 
 const play = document.getElementById("play")
 
-const randomNumber = Math.floor(Math.random() * 5 ) + 1;
-console.log(`numero random è ${randomNumber}`);
-
 evenB.addEventListener("click", function(){
-  calculate("even")
-  console.log("hai scelto pari")
+  calculate("even");
+  console.log("hai scelto pari");
 });
 
 oddB.addEventListener("click", function(){
-  calculate("odd")
-  console.log("hai scelto dispari")
+  calculate("odd");
+  console.log("hai scelto dispari");
 });
 
 play.addEventListener("click", function(){
   const userNumber = parseInt(userSelectedNumber.value); //RISOLTO, ho aggiuto il parseint, idea delle 1.35 di notte
+  const randomNumber = Math.floor(Math.random() * 5 ) + 1;
+  console.log(randomNumber)
   if (userNumber >= 1 && userNumber <= 5){
-    console.log("puoi continuare")
+    calculate(userNumber);
   } else {
     alert("Inserisci un numero valido")
   }
 })
+
+if ((randomNumber + userNumber) % 2 === 0) {
+  console.log("a")
+} 
+
+//niente, ci ho rinunciato
 
 
 
